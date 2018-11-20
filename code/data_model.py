0cd5a024b90e349ce6b56d96696d2db75f09925d
kael_w <kael_w@yeah.net>
2018-11-20 20:39:56 +0800

    11/20

--- code/data_model.py
+++ code/data_model.py
@@ -1,9 +1,62 @@
 # -*- coding:utf-8 -*-
 
-import time
 import re
 import pickle
-android_event_type = ['TYPE_WINDOW_STATE_CHANGED', 'TYPE_WINDOW_CONTENT_CHANGED', 'TYPE_VIEW_FOCUSED', 'TYPE_VIEW_SCROLLED', 'TYPE_VIEW_CLICKED', 'TYPE_VIEW_TEXT_SELECTION_CHANGED', 'TYPE_VIEW_ACCESSIBILITY_FOCUSED', 'TYPE_VIEW_TEXT_CHANGED', 'TYPE_VIEW_SELECTED', 'TYPE_NOTIFICATION_STATE_CHANGED', 'TYPE_ANNOUNCEMENT', 'TYPE_VIEW_ACCESSIBILITY_FOCUS_CLEARED', 'TYPE_VIEW_LONG_CLICKED', 'TYPE_VIEW_HOVER_ENTER', 'TYPE_VIEW_HOVER_EXIT']
+android_event_type_c = {
+    'TYPE_WINDOW_STATE_CHANGED':'g',
+    'TYPE_WINDOW_CONTENT_CHANGED':'b',
+    'TYPE_VIEW_FOCUSED':[0.8,0.1,0.8],
+    'TYPE_VIEW_SCROLLED':'k',
+    'TYPE_VIEW_CLICKED':'c',
+    'TYPE_VIEW_TEXT_SELECTION_CHANGED':[0.4,0.4,0.4],
+    'TYPE_VIEW_ACCESSIBILITY_FOCUSED':[0.1,0.8,0.1],
+    'TYPE_VIEW_TEXT_CHANGED':[0.4,0.1,0.1],
+    'TYPE_VIEW_SELECTED':[0.1,0.1,0.4],
+    'TYPE_NOTIFICATION_STATE_CHANGED':'y',
+    'TYPE_ANNOUNCEMENT':[0.8,0.8,0.8],
+    'TYPE_VIEW_ACCESSIBILITY_FOCUS_CLEARED':[0.8,0.4,0.8],
+    'TYPE_VIEW_LONG_CLICKED':[0.5,0,0.5],
+    'TYPE_VIEW_HOVER_ENTER':[0.8,0.1,0.1],
+    'TYPE_VIEW_HOVER_EXIT':[0.8,0.8,0.1],
+}
+
+android_event_type = {
+    'TYPE_WINDOW_STATE_CHANGED':[],
+    'TYPE_WINDOW_CONTENT_CHANGED':[],
+    'TYPE_VIEW_FOCUSED':[],
+    'TYPE_VIEW_SCROLLED':[],
+    'TYPE_VIEW_CLICKED':[],
+    'TYPE_VIEW_TEXT_SELECTION_CHANGED':[],
+    'TYPE_VIEW_ACCESSIBILITY_FOCUSED':[],
+    'TYPE_VIEW_TEXT_CHANGED':[],
+    'TYPE_VIEW_SELECTED':[],
+    'TYPE_NOTIFICATION_STATE_CHANGED':[],
+    'TYPE_ANNOUNCEMENT':[],
+    'TYPE_VIEW_ACCESSIBILITY_FOCUS_CLEARED':[],
+    'TYPE_VIEW_LONG_CLICKED':[],
+    'TYPE_VIEW_HOVER_ENTER':[],
+    'TYPE_VIEW_HOVER_EXIT':[]
+}
+
+android_logcat_type_c = {
+    'V':'g',
+    'D':'r',
+    'I':'c',
+    'W':'y',
+    'E':'k',
+    'F':'w',
+    'S':'m'
+}
+
+android_logcat_type = {
+    'V':[],
+    'D':[],
+    'I':[],
+    'W':[],
+    'E':[],
+    'F':[],
+    'S':[]
+}
 
 import os
 import matplotlib.pyplot as plt
@@ -26,26 +79,35 @@ def file_name(file_dir):
 r=0
 logcat=[]
 event = []
-files=file_name('D:\Github\S-CAT\code\data\com.example.myfristandroid')
+files=file_name('.\data\com.example.myfristandroid')
 
-print(android_event_type)
 
 # for f in files:
-logcat_file = open('D:\Github\S-CAT\code\data\com.example.myfristandroid\\'+'787_1'+'\logcat.pkl',"rb")
-event_file = open('D:\Github\S-CAT\code\data\com.example.myfristandroid\\'+'787_1'+'\event.pkl',"rb")
+logcat_file = open('.\data\com.example.myfristandroid\\'+'695_1'+'\logcat.pkl',"rb")
+event_file = open('.\data\com.example.myfristandroid\\'+'695_1'+'\event.pkl',"rb")
 logcat_list = pickle.load(logcat_file)
 event_list = pickle.load(event_file)
-print(event_list)
-print(logcat_list)
+
+for e in event_list:
+    android_event_type[e['EventType']].append(e)
+for l in logcat_list:
+    android_logcat_type[l['priority']].append(l)
+
 char_zhi = min(event_list[0]['SyscTime'],logcat_list[0]['SyscTime'])
 
-event_time_list = [float(x['SyscTime']-char_zhi) for x in event_list]
-logcat_time_list = [float(x['SyscTime']-char_zhi) for x in logcat_list]
-#print(event_time_list[0])
-print(logcat_time_list)
 fig = plt.figure('fig')
-plt.scatter(logcat_time_list,[x for x in range(len(logcat_time_list))],c='r',marker='o',label="logcat")
-plt.scatter(event_time_list,[x for x in range(len(event_time_list))],c='g',marker='v',label="event")
+i = 200
+
+for a in android_logcat_type:
+    logcat_time_list = [float(x['SyscTime']-char_zhi) for x in android_logcat_type[a]]
+    if len(logcat_time_list)>0:
+        plt.scatter(logcat_time_list,[100 for x in range(i,i+len(logcat_time_list))],c=android_logcat_type_c[a],marker='o',label=a)
+        i+=100
+for a in android_event_type:
+    event_time_list = [float(x['SyscTime'] - char_zhi) for x in android_event_type[a]]
+    if len(event_time_list)>0:
+        plt.scatter(event_time_list,[x for x in range(i,i+len(event_time_list))],c=android_event_type_c[a],marker='v',label=a)
+        i+=100
 plt.title("data")
 # plt.xlim(110000,125000)
 # plt.ylim(0,200)
